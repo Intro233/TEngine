373c90d44419842ba1f5efbcc81ed4ee53469f35
WangJunCheng <2838608385@qq.com>
2026-08-26 17:25:36 +0800

    新增快速打开配置表根目录

--- UnityProject/Assets/TEngine/Editor/LubanTools/LubanTools.cs
+++ UnityProject/Assets/TEngine/Editor/LubanTools/LubanTools.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -5,16 +6,32 @@ namespace TEngine.Editor
 {
     public static class LubanTools
     {
+        private static string GameConfigRoot =>
+            Path.GetFullPath(Path.Combine(Application.dataPath, "../../Configs/GameConfig"));
+
         [MenuItem("TEngine/Luban/转表 &X", priority = -100)]
         private static void ZhuanXiaoYi()
         {
 #if UNITY_EDITOR_OSX || UNITY_EDITOR_LINUX
-            string path = Application.dataPath + "/../../Configs/GameConfig/gen_code_bin_to_project_lazyload.sh";
+            string path = Path.Combine(GameConfigRoot, "gen_code_bin_to_project_lazyload.sh");
 #elif UNITY_EDITOR_WIN
-            string path = Application.dataPath + "/../../Configs/GameConfig/gen_code_bin_to_project_lazyload.bat";
+            string path = Path.Combine(GameConfigRoot, "gen_code_bin_to_project_lazyload.bat");
 #endif
             Debug.Log($"执行转表：{path}");
             ShellHelper.RunByPath(path);
+        }
+
+        [MenuItem("TEngine/Luban/打开表目录", priority = -99)]
+        private static void OpenGameConfigFolder()
+        {
+            string path = GameConfigRoot;
+            if (!Directory.Exists(path))
+            {
+                Debug.LogError($"表目录不存在：{path}");
+                return;
+            }
+
+            OpenFolderHelper.Execute(path);
         }
     }
 }
